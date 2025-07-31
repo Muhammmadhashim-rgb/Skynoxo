13a00fe0fca5971fee0df78ab0b7b4c2711049d7
Muhammmadhashim-rgb <muhammadhashim4412@gmail.com>
2025-07-31 21:28:13 +0500

    add TimeForecast

--- src/componets/ForecastDetial/Forecast.jsx
+++ src/componets/ForecastDetial/Forecast.jsx
@@ -1,14 +1,33 @@
 import React, { Component } from 'react'
 import './Forecast.css';  // ✅ CSS File Imported
+import TimeForecast from './TimeForacast';
+
 
 export default class Forecast extends Component {
    render() {
       return (
          <div className='Forecast_background'>
             <h1 className='Forecast_heading'>Today / Week</h1>
-
             <div className="oneHoureForecast">
-                     
+               <TimeForecast time="1PM" icon="/images/time_Cloud.svg" unit="20"/>
+               <TimeForecast time="2PM" icon="/images/time_Cloud.svg" unit="21"/>
+               <TimeForecast time="3PM" icon="/images/time_Cloud.svg" unit="18"/>
+               <TimeForecast time="4PM" icon="/images/time_Cloud.svg" unit="23"/>
+               <TimeForecast time="5PM" icon="/images/time_Cloud.svg" unit="27"/>
+               <TimeForecast time="6PM" icon="/images/time_Cloud.svg" unit="30"/>
+               <TimeForecast time="7PM" icon="/images/time_Cloud.svg" unit="19"/>
+            </div>
+            <div className="tomorrow_Forecast">
+               <h1 className='tomorrow_Forecast_heading'>
+                  Tomorrow
+               </h1>
+         <p className='tomorrow_Forecast_para'>
+            Thunder storm
+         </p>
+         <h2 className='tomorrow_Forecast_unit'>
+            14°
+         </h2>
+         <img className='thunderstorm_cloud' src="/images/thunderstorm_cloud.gif" alt="" />
             </div>
          </div>
 
---
+++ src/componets/ForecastDetial/TimeForacast.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import './TimeForacast.css';
+
+function TimeForacast(props) {
+     return (
+      <div>
+        <div className='timeForacast_card'>
+         <h1 className='Foracast_time'>{props.time}</h1>
+         <img className='timeForacast_cloud' src={props.icon} alt="" />
+          <h1 className='timeForacast_unit'>{props.unit}°</h1>
+        </div>
+      </div>
+    )
+}
+
+
+export default  TimeForacast
