28ec7326c15a722079116656c795e49dcc07e37d
muhammmadhashim-rgb <muhammadhashim4412@gmail.com>
2025-08-05 03:38:05 +0500

    design user

--- src/componets/User Name/User.jsx
+++ src/componets/User Name/User.jsx
@@ -5,21 +5,19 @@ export default class User extends Component {
    render() {
       return (
          <div className='userNameDropDown'>
-
             <div className='user'>
-
                <nav className="navbar">
+                  <img  src="/images/user_icon.svg" alt="User Icon" />
+                  <h1 className='userHeading'>User Name</h1>
                   <ul>
                      <li>
-                        <a href="#">▾</a>
+                        <img className='Dropdown' src="/images/DropDown.svg" alt="Dropdown Icon" />
                         <div className="dropdown-content">
                            <a href="#">Web Design</a>
                            <a href="#">Development</a>
                            <a href="#">SEO</a>
                         </div>
                      </li>
-
-
                   </ul>
                </nav>
             </div>
